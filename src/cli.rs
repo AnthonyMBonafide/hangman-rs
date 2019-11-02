cddf61b0c212e9d4f332fbca5b0b8241a1667bb7
Anthony M. Bonafide <AnthonyMBonafide@gmail.com>
2019-11-02 08:42:39 -0400

    Move CLI parsing to module
    Signed-off-by: Anthony M. Bonafide <AnthonyMBonafide@gmail.com>

---
+++ src/cli.rs
@@ -0,0 +1,30 @@
+use crate::hangman::HangmanGame;
+use crate::dictionary;
+use clap::{App, Arg};
+
+pub fn read_cli_args() -> HangmanGame{
+
+    // Parse CLI command arguments
+    let matches = App::new("Hangman")
+        .version("1.0")
+        .author("Anthony M. Bonafide <AnthonyMBonafide@gmail.com>")
+        .about("A CLI version of hangman.")
+        .arg(
+            Arg::with_name("difficulty")
+                .short("d")
+                .long("difficulty")
+                .value_name("difficulty")
+                .help("Sets the difficulty of the game")
+                .possible_values(&["easy".as_ref(), "medium".as_ref(), "hard".as_ref()])
+                .default_value("easy")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    // Get the difficulty from the parse CLI arguments.
+    let difficulty = matches.value_of("difficulty").unwrap_or("easy");
+    println!("Value for difficulty: {}", difficulty);
+
+
+    HangmanGame::new(dictionary::SimpleWordGenerator {})
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,15 @@
-use clap::{App, Arg};
 use hangman::{HangmanGame, State};
 use std::io::{self, BufRead};
+use crate::cli::read_cli_args;
 
 mod dictionary;
 mod display;
 mod hangman;
+mod cli;
 
 fn main() {
-    // Parse CLI command arguments
-    let matches = App::new("Hangman")
-        .version("1.0")
-        .author("Anthony M. Bonafide <AnthonyMBonafide@gmail.com>")
-        .about("A CLI version of hangman.")
-        .arg(
-            Arg::with_name("difficulty")
-                .short("d")
-                .long("difficulty")
-                .value_name("difficulty")
-                .help("Sets the difficulty of the game")
-                .possible_values(&["easy".as_ref(), "medium".as_ref(), "hard".as_ref()])
-                .default_value("easy")
-                .takes_value(true),
-        )
-        .get_matches();
-
-    // Get the difficulty from the parse CLI arguments.
-    let difficulty = matches.value_of("difficulty").unwrap_or("easy");
-    println!("Value for difficulty: {}", difficulty);
-
     // Create a game with the word to guess based on the difficulty.
-    let mut game = HangmanGame::new(dictionary::SimpleWordGenerator {});
+    let mut game = read_cli_args();
 
     // Main game loop
     while match game.get_state() {
